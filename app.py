8870d67bd1d4df674168cb968cb4b4344f5503bc
MichaelWHamilton <mwhamilton@ualr.edu>
2025-03-03 17:43:01 -0600

    testing some stuff

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from server import app, socketio
+from flask import render_template
 
-app = Flask(__name__)
-socketio = SocketIO(app)
+# app = Flask(__name__)
+# socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html') # chat UI
+# @app.route('/')
+# def index():
+#     return render_template('index.html') # chat UI
 
-@socketio.on('message')
-def handle_message(msg):
-    print(f"Message received: {msg}")
-    send(msg, broadcast=True) # broadcasts the message to all clients
+# @socketio.on('message')
+# def handle_message(msg):
+#     print(f"Message received: {msg}")
+#     send(msg, broadcast=True) # broadcasts the message to all clients
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
+
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- server/server.py
+++ server/server.py
@@ -1,1 +1,19 @@
- 
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__, template_folder='../templates')
+
+socketio = SocketIO(app);
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Received message: {msg}")
+    socketio.send(msg)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
