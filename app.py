1136ec23cc6f6bc495d359359e9870b218824369
MichaelWHamilton <mwhamilton@ualr.edu>
2025-03-03 18:05:23 -0600

    file structure change/server file handling server side stuff

--- app.py
+++ app.py
@@ -1,22 +1,2 @@
 from server import app, socketio
 from flask import render_template
-
-# app = Flask(__name__)
-# socketio = SocketIO(app)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html') # chat UI
-
-# @socketio.on('message')
-# def handle_message(msg):
-#     print(f"Message received: {msg}")
-#     send(msg, broadcast=True) # broadcasts the message to all clients
-
-# if __name__ == '__main__':
-#     socketio.run(app, debug=True)
-
-
-
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import uuid
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app = Flask(__name__)
 
 socketio = SocketIO(app);
 
